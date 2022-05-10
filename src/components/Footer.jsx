ee5a4899fc7b19df21f02bcce425e730e6d856c0
lconnell1000 <lconnell1000@gmail.com>
2022-05-10 22:50:35 +0930

    working on product list page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,107 @@
+import { Facebook, Instagram, MailOutline, Phone, RoomOutlined } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+`
+const Logo = styled.h1`
+
+`
+const Description = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 30px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+
+`
+
+const Center = styled.div`
+    flex: 1;
+    padding: 15px;
+`
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 15px;
+`
+const Right = styled.div`
+    flex: 1;
+    padding: 15px;
+`
+const ContactItem = styled.div`
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+`
+const Payment = styled.img`
+    width: 75%;
+`
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>AGAIN CLOTHING.</Logo>
+            <Description>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quis reprehenderit facere laudantium aut. Sequi beatae quisquam aperiam et earum, odio quaerat, esse est cum corrupti culpa unde consectetur corporis.</Description>
+            <SocialContainer>
+                <SocialIcon color="3B5999">
+                    <Facebook/>
+                </SocialIcon>
+                <SocialIcon color="E4405F">
+                    <Instagram/>
+                </SocialIcon>
+            </SocialContainer>
+        </Left>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Upper Garments</ListItem>
+            <ListItem>Lower Garments</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms & Conditions</ListItem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem>
+                <RoomOutlined style={{marginRight:"10px"}}/>9 Gothic Ave Stonyfell, SA 5066</ContactItem>
+            <ContactItem>
+                <Phone style={{marginRight:"10px"}}/>+61 423 633 515</ContactItem>
+            <ContactItem>
+                <MailOutline style={{marginRight:"10px"}}/>fionaconnell@gmail.com</ContactItem>
+            <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+        </Right>
+    </Container>
+  )
+}
+
+export default Footer
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,23 +1,41 @@
 import { Send } from "@material-ui/icons"
-import styledComponents from "styled-components"
+import styled from "styled-components"
 
 const Container = styled.div`
-
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `
 const Title = styled.h1`
-
+    font-size: 60px;
+    margin-bottom: 15px;
 `
 const Decription = styled.div`
-
+    font-size: 24px;
+    font-weight: 350;
+    margin-bottom: 15px;
 `
 const InputContainer = styled.div`
-
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
 `
 const Input = styled.input`
-
+    border: none;
+    flex: 8;
+    padding-left: 15px;
 `
 const Button = styled.button`
-
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
 `
 const Newsletter = () => {
   return (
