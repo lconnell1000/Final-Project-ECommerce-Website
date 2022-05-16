1d9df62eb435ce82dc94e25228985b3d9d366af8
lconnell1000 <lconnell1000@gmail.com>
2022-05-16 12:25:32 +0930

    product routes completed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 const authRoute = require("./routes/auth")
 dotenv.config();
 
@@ -15,6 +16,7 @@ console.log(err);
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server is running!");
