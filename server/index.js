70a57ae951a98884dc8c214b9ef818d6151febd0
lconnell1000 <lconnell1000@gmail.com>
2022-05-16 14:36:18 +0930

    cart and order routes finished

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
-const authRoute = require("./routes/auth")
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend server is running!");
