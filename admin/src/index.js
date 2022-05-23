43dbb3df4b82f3a80c3660b34780f91ef84f119f
lconnell1000 <lconnell1000@gmail.com>
2022-05-23 13:38:36 +0930

    got functionality for admin page happening

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react"
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store = {store}>
-    <PersistGate loading="null" persistor={persistor}>
-    <App />
+  <Provider store={store}>
+   <PersistGate loading="null" persistor={persistor}>
+      <App />
     </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
