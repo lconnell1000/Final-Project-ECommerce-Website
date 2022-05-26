04e72824d55df42a57bc18101abad4b56f46838c
lconnell1000 <lconnell1000@gmail.com>
2022-05-26 15:59:54 +0930

    put dollars infront of orders on admin page

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -35,7 +35,7 @@ getOrders();
             <span className="widgetLgName">{order.userId}</span>
           </td>
           <td className="widgetLgDate">{format(order.createdAt)}</td>
-          <td className="widgetLgAmount">{order.amount}</td>
+          <td className="widgetLgAmount">${order.amount}</td>
           <td className="widgetLgStatus">
             <Button type={order.status} />
           </td>
